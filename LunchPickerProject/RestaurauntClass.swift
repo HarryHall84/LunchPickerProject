dd4302d95f5c14c001718ffa32308dcae123916d
Harrison Hall <harrison.hall23@d155.org>
2022-01-10 08:35:13 -0600

    RestaurauntClass added

---
+++ LunchPickerProject/RestaurauntClass.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurauntClass.swift
+//  LunchPickerProject
+//
+//  Created by Harrison Hall on 1/10/22.
+//
+
+import Foundation
--- LunchPickerProject/ViewController.swift
+++ LunchPickerProject/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+// Title screen 
 class ViewController: UIViewController {
     // Initial Program
     override func viewDidLoad() {
