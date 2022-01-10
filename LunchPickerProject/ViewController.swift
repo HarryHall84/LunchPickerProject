f950d998d1c9fd239638c27ed4984edef52f1fb2
Harrison Hall <harrison.hall23@d155.org>
2022-01-10 08:22:22 -0600

    Comment Added (Initial Commit)

--- LunchPickerProject/ViewController.swift
+++ LunchPickerProject/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Initial Program
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
