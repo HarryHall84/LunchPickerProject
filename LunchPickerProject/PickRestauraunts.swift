ea2a16980d904771a868d760e3c4cec0049911f1
Mateo Acosta <mateo.acosta22@d155.org>
2022-01-13 08:04:38 -0600

    lesssssss gooooooo

--- LunchPickerProject/PickRestauraunts.swift
+++ LunchPickerProject/PickRestauraunts.swift
@@ -18,9 +18,20 @@ class PickRestauraunts: UIViewController, UICollectionViewDataSource, UICollecti
         var mcdonalds = Resturant(adresss: "551 Crystal Point Rd", phoneN: "815-455-6630", restName: "Mcdonalds", distance: 10, resturantLogo: "clowning")
         var burgerKing = Resturant(adresss: "250 W Virginia St, Crystal Lake, IL 60014", phoneN: "815-459-2976", restName: "Burger King", distance: 22, resturantLogo: "burgerKing")
         var culvers = Resturant(adresss: "400 Pingree Rd Crystal Lake, IL 60014", phoneN: "815-356-9090", restName: "Culvers", distance: 10, resturantLogo: "culvers.png")
-        var popeys = Resturant(adresss: <#T##String#>, phoneN: <#T##String#>, restName: <#T##String#>, distance: <#T##Int#>, resturantLogo: <#T##String#>)
-        var chickFille = Resturant(adresss: <#T##String#>, phoneN: <#T##String#>, restName: <#T##String#>, distance: <#T##Int#>, resturantLogo: <#T##String#>)
+        var popeys = Resturant(adresss: "340 W. Virginia St.Crystal Lake, IL, 60014", phoneN: "779-220-4934", restName: "Popey's", distance: 10, resturantLogo: "popeyes")
+        var chickFille = Resturant(adresss: "4812 Northwest Hwy,Crystal Lake, IL 60014", phoneN: "815-444-8611", restName: "chick-fill-a", distance: 10, resturantLogo:"chick fill a" )
+        var subway = Resturant(adresss: "6166 Northwest Hwy, Crystal Lake, IL 60014", phoneN: "815-455-3535", restName: "Subway", distance: 10, resturantLogo: "subway")
+        var jimmyjohns = Resturant(adresss: "5657 Northwest Hwy, Crystal Lake, IL 60014", phoneN: "815-455-2555", restName: "Jimmy Johns", distance: 10, resturantLogo: "jimmyJohns")
+        var tommys = Resturant(adresss: "363 W Virginia St, Crystal Lake, IL 60014", phoneN: "815-455-4966", restName: "Tommy's", distance: 10, resturantLogo: "tommys")
         resturantArray.append(culvers)
+        resturantArray.append(tacoBell)
+        resturantArray.append(mcdonalds)
+        resturantArray.append(burgerKing)
+        resturantArray.append(popeys)
+        resturantArray.append(chickFille)
+        resturantArray.append(subway)
+        resturantArray.append(jimmyjohns)
+        resturantArray.append(tommys)
         collectionViewOutlet.delegate = self
         collectionViewOutlet.dataSource = self
         // Do any additional setup after loading the view.
