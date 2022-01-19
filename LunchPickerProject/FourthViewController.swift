f3031285d2cd87ea759ac4f1d58c69fcab29e0d0
Mateo Acosta <mateo.acosta22@d155.org>
2022-01-19 08:07:21 -0600

    yeyyeye

---
+++ LunchPickerProject/FourthViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FourthViewController.swift
+//  LunchPickerProject
+//
+//  Created by mateoacosta on 1/19/22.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    @IBOutlet weak var adressOut: UILabel!
+    @IBOutlet weak var phoneNumberOut: UILabel!
+    @IBOutlet weak var distanceOut: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
