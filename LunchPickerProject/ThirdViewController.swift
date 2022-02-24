d36b605c3a046efe3d8ab906e4d327dc8b89569a
Harrison Hall <harrison.hall23@d155.org>
2022-02-24 07:31:20 -0600

    Wheel added - needs fixing

--- LunchPickerProject/ThirdViewController.swift
+++ LunchPickerProject/ThirdViewController.swift
@@ -7,16 +7,74 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     var selectedResturants2 : [Resturant] = []
     var finalResturant : Resturant!
+    @IBOutlet weak var pick: UIPickerView!
     @IBOutlet weak var resturantNamy: UILabel!
+    var myT = Timer()
+    var rand = Int.random(in: 0...10)
     
+     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pick.delegate = self
+        pick.dataSource = self
+        
+        pick.reloadAllComponents()
+       // print(myArr)
+            myT = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(ThirdViewController.movePicker), userInfo: nil, repeats: true)
+        print("Number Generated: \(rand)")
+
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    //MARK: - move picker
+
+    @objc func movePicker()  {
+
+
+        let position = Int(arc4random_uniform(89) + 10)
+
+
+
+        pick.selectRow(position, inComponent: 0, animated: true)
+        pick.showsSelectionIndicator = true
+        if position == rand {
+
+            myT.invalidate()
+
+            let alert = UIAlertController(title: "You Won!!", message: "Congratulations!!!", preferredStyle: .alert)
+            let buttonOK = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+
+            alert.addAction(buttonOK)
+
+            present(alert, animated: true, completion: nil)
+
+
+
+        }
+
+    }
+
+    
+    //MARK: - picker
+   func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return 1
+
+   }
+
+   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       return selectedResturants2.count
+       
+   }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(selectedResturants2[row].restName)"
+    }
+    
     
     func randomizeResturants(){
         var randdoNum = selectedResturants2.count
@@ -25,15 +83,6 @@ class ThirdViewController: UIViewController {
     }
     
     
-    @IBAction func spinBoy(_ sender: UITapGestureRecognizer) {
-        print("count: \(selectedResturants2.count)")
-        randomizeResturants()
-        print("\(finalResturant.restName)")
-        let alert = UIAlertController(title: "Wheel Spun", message: "The wheel selected: \(finalResturant.restName)", preferredStyle: .actionSheet)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let nvc = segue.destination as? FourthViewController
