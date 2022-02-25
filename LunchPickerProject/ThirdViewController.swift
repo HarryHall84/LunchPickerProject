11b6c647a2b8ad0028584d590781e2a287aee5e8
Harrison Hall <harrison.hall23@d155.org>
2022-02-25 07:42:13 -0600

    Spinner ADDED

--- LunchPickerProject/ThirdViewController.swift
+++ LunchPickerProject/ThirdViewController.swift
@@ -14,20 +14,45 @@ class ThirdViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
     @IBOutlet weak var resturantNamy: UILabel!
     var myT = Timer()
     var rand = Int.random(in: 0...10)
-    
-     
+    var countNum = 0
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         pick.delegate = self
         pick.dataSource = self
         
+      
         pick.reloadAllComponents()
-       // print(myArr)
-            myT = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(ThirdViewController.movePicker), userInfo: nil, repeats: true)
+        myT = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(ThirdViewController.movePicker), userInfo: nil, repeats: true)
         print("Number Generated: \(rand)")
-
-
+        var i = 0
+        // Array 2-5, i = 4, Array 6-9, i = 3
+         while i < 4 {
+        for i in selectedResturants2 {
+            selectedResturants2.append(i)
+        }
+            i += 1
+        }
+        print("Stuff created: \(selectedResturants2.count)")
         // Do any additional setup after loading the view.
+    }
+    
+    func endSpinner(){
+        countNum += 1
+        print("countNum is at: \(countNum)")
+        if countNum == 1 {
+            
+        myT.invalidate()
+
+            let alert = UIAlertController(title: "Resturaunt Selected", message: "It selected: \(selectedResturants2[rand].restName)", preferredStyle: .alert)
+        let buttonOK = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+
+
+        alert.addAction(buttonOK)
+
+        present(alert, animated: true, completion: nil)
+
+        }
     }
     
     
@@ -35,27 +60,42 @@ class ThirdViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
 
     @objc func movePicker()  {
 
-
-        let position = Int(arc4random_uniform(89) + 10)
-
-
-
+        let intCount = selectedResturants2.count
+        let position = Int(arc4random_uniform(UInt32(intCount)) + 0)
+        print(position)
+        
+        
         pick.selectRow(position, inComponent: 0, animated: true)
-        pick.showsSelectionIndicator = true
-        if position == rand {
-
-            myT.invalidate()
-
-            let alert = UIAlertController(title: "You Won!!", message: "Congratulations!!!", preferredStyle: .alert)
-            let buttonOK = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-
-            alert.addAction(buttonOK)
-
-            present(alert, animated: true, completion: nil)
-
-
-
+        switch selectedResturants2.count {
+        case 72: // Initial array has 9
+            if position == rand || position == rand + 9 || position == rand + 18 || position == rand + 27 || position == rand + 36 || position == rand + 45 || position == rand + 54 || position == rand + 63 || position == rand + 72 {
+                endSpinner()
+            }
+        case 64: // Initial array has 8 or 4
+            if position == rand || position == rand + 8 || position == rand + 16 || position == rand + 24 || position == rand + 32 || position == rand + 40 || position == rand + 48 || position == rand + 56 || position == rand + 64 {
+                endSpinner()
+            }
+        case 56: // Array 7
+            if position == rand || position == rand + 7 || position == rand + 14 || position == rand + 21 || position == rand + 28 || position == rand + 35 || position == rand + 42 || position == rand + 49 || position == rand + 56 {
+                endSpinner()
+            }
+        case 48: // Array 6 or 3
+            if position == rand || position == rand + 6 || position == rand + 12 || position == rand + 18 || position == rand + 24 || position == rand + 30 || position == rand + 36 || position == rand + 42 || position == rand + 48 {
+                endSpinner()
+            }
+        case 80: // Array 5
+            if position == rand || position == rand + 5 || position == rand + 10 || position == rand + 15 || position == rand + 20 || position == rand + 25 || position == rand + 30 || position == rand + 35 || position == rand + 40 || position == rand + 45 || position == rand + 50 || position == rand + 55 || position == rand + 60 || position == rand + 65 || position == rand + 70 || position == rand + 75 || position == rand + 80 {
+                endSpinner()
+            }
+        case 32:
+            if position == rand || position == rand + 2 || position == rand + 4 || position == rand + 6 || position == rand + 8 || position == rand + 10 || position == rand + 12 || position == rand + 14 || position == rand + 16 || position == rand + 18 || position == rand + 20 || position == rand + 22 || position == rand + 24 || position == rand + 26 || position == rand + 28 || position == rand + 30 || position == rand + 32 {
+                endSpinner()
+            }
+        
+        default:
+            break
         }
+  
 
     }
 
